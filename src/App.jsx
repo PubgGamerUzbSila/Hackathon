afdfd42867d9cdcfa03af600341b4ec9ad0aa9c3
Asliddin001 <asultonov040@gmail.com>
2023-11-21 19:25:45 +0500

    i created footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import ProfService from "./components/ProfService/ProfService";
 import { Hero } from "./components/Hero/Hero";
 import CleaningSide from "./components/OurCleaning/CleaningSide";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
