8225a8e8b10a1f67a826126a003bd0b444eb90e2
umidjonhp <umidjonmamasharifov@gmail.com>
2023-11-21 18:05:07 +0500

    Umid's section end

--- src/components/Sumbit/Sumbit.jsx
+++ src/components/Sumbit/Sumbit.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './Sumbit.module.css'
 import { IoMdCall } from "react-icons/io";
+
 const Sumbit = () => {
   return (
     <>
@@ -9,13 +10,16 @@ const Sumbit = () => {
                 <h2>Request a free cleaning quote today</h2>
                 <p>In dignissim euismod pretium amet enim a eu nam ut urna accumsan pellentesque lacus duis pharetra eutortor.</p>
                 <div className={styles.call}>
-                    <span><IoMdCall /></span>
+                    <span className={styles.icon}><IoMdCall /></span>
                     <div className={styles.callNum}>
                         <span>Call us now</span>
                         <span>(414) 567 - 2109</span>
                     </div>
                 </div>
-
+                <div className={styles.line}></div>
+                <h3>Not convinced yet?</h3>
+                <p>Massa bibendum consectetur maurisid gravida purus, dolor dui amet morbi non nunc urna purus diam.</p>
+                <button>Browse our packages</button>
             </div>
             <form>
 
