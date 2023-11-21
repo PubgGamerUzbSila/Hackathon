66939ddb0b3478184b117e30e0d7ea9435a0c5de
Muzaffar <mamanazarovmuzaffar07@gmail.com>
2023-11-21 18:13:04 +0500

    change button type2 and OurCleaning

---
+++ src/components/OurCleaning/CleaningSide.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import styles from './CleaaningSide.module.css'
+import home from '../../assets/img/IMAGE.png'
+import true_img from '../../assets/icons/true.svg'
+import { Button } from '../UI/Button/Button'
+
+function CleaningSide() {
+  return (
+    <div className={styles.cleaning}>
+        <div className={styles.left}>
+            <img src={home} alt="" />
+        </div>
+        <div className={styles.right}>
+            <h2>We cover all areas of your home or office</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+            <div className={styles.trues}>
+                <div>
+                    <img src={true_img} alt="" />
+                    <span>Bathrooms</span>
+                </div>
+                <div>
+                    <img src={true_img} alt="" />
+                    <span>Kitchens</span>
+                </div>
+                <div>
+                    <img src={true_img} alt="" />
+                    <span>Living Rooms</span>
+                </div>
+                <div>
+                    <img src={true_img} alt="" />
+                    <span>Carpets</span>
+                </div>
+                <div>
+                    <img src={true_img} alt="" />
+                    <span>Bedrooms</span>
+                </div>
+                <div>
+                    <img src={true_img} alt="" />
+                    <span>Offices</span>
+                </div>
+                <div>
+                    <img src={true_img} alt="" />
+                    <span>Businesses</span>
+                </div>
+                <div>
+                    <img src={true_img} alt="" />
+                    <span>Windows</span>
+                </div>
+            </div>
+            <Button padding="padding__type2">Get a free quote</Button>
+        </div>
+    </div>
+  )
+}
+
+export default CleaningSide
