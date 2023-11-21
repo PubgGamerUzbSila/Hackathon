df870e7baf1db026699140a85bae4cfb37ee029a
sabina <sabinatogayeva0@gmail.com>
2023-11-21 18:51:20 +0500

    Sabina's section the end

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import OurCleaning from "./components/OurCleaning/OurCleaning";
 
 function App() {
   return (
     <>
       <div className="container">
-
+     <OurCleaning/>
       </div>
     </>
   );
