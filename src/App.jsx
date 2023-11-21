6d22f1843e43ada78c1405b2c7b6cb7340616319
Asliddin001 <asultonov040@gmail.com>
2023-11-21 17:47:02 +0500

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import "./App.css";
-import { Button } from "./components/UI/Button/Button";
 
 function App() {
   return (
     <>
       <div className="container">
-        <Button padding="padding__type1">Get a free quote</Button>
       </div>
     </>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,37 +1,45 @@
 import React from "react";
+//container uchun
+import "../../App.css"
+//component uchun
 import styles from "./Navbar.module.css";
 //Logo
 import logo from "../../assets/icons/logo.svg";
+import { Button } from "../UI/Button/Button";
 
 const Navbar = () => {
   return (
     <>
-      <nav>
-        <div className={styles.logo}>
-          <img src={logo} alt="logo" />
-        </div>
-        <ul>
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">About</a>
-          </li>
-          <li>
-            <a href="#">Services</a>
-          </li>
-          <li>
-            <a href="#">Pages</a>
-          </li>
-          <li>
-            <a href="#">Contact</a>
-          </li>
-        </ul>
-        <div className={styles.btns}>
-          <p>Cart (0)</p>
-          <Button></Button>
-        </div>
-      </nav>
+      <div className="container">
+        <nav>
+          <div className={styles.links}>
+            <div className={styles.logo}>
+              <img src={logo} alt="logo" />
+            </div>
+            <ul>
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a href="#">Services</a>
+              </li>
+              <li>
+                <a href="#">Pages</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+            </ul>
+          </div>
+          <div className={styles.btns}>
+            <p>Cart (0)</p>
+            <Button padding="padding__type1">Get a free quote</Button>
+          </div>
+        </nav>
+      </div>
     </>
   );
 };
