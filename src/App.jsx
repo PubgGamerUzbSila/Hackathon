a3b382dccd84b5fe070940c4b7092893f2f4eeb5
Shahboz <leo@shakhboz.local>
2023-11-21 18:18:41 +0500

    created our service

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import OurService from "./components/OurService/OurService";
 
 function App() {
   return (
     <>
       <div className="container">
-
+        <OurService/>
       </div>
     </>
   );
