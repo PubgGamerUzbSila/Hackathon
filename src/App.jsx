ecbb03465b1b86c919119a09d6e5ddb1c3a57798
Sattorov Kamronbek <pubggameruzb.sila@gmail.com>
2023-11-21 18:19:14 +0500

    fixed bags

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,13 @@ import ProfService from "./components/ProfService/ProfService";
 import { Hero } from "./components/Hero/Hero";
 import CleaningSide from "./components/OurCleaning/CleaningSide";
 
-
 function App() {
   return (
     <>
-      <div className="container">
-        <ProfService/>
+      <div className='container'>
         <Hero />
-        <CleaningSide/>
+        <ProfService />
+        <CleaningSide />
       </div>
     </>
   );
